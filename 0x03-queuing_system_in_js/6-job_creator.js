9d8a62ab19b2acadbd316525ebc6aa69941929c6
hashkitti001 <iJeddy5.0@gmail.com>
2024-12-02 14:48:46 +0100

    add task 6 to create queue and handle queue events with Kue

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -1,1 +1,22 @@
-import 
+import { createQueue } from "kue";
+const queue = createQueue({ name: 'push_notification_code' })
+
+const job = queue.create({
+    phoneNumber: "+233932939293",
+    message: "Staying alive!!!",
+})
+
+
+job.on('enqueue', () => {
+    console.log('Notification job created:', job.id);
+})
+
+job.on('complete', () => {
+    console.log('Notification job completed')
+})
+
+job.on('failed attempt', () => {
+    console.log('Notification job failed')
+})
+
+job.save()
